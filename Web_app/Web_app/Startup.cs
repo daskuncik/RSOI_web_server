7c7505f13cc029f39eb096eeb2541ec1f4a182f5
Solomatina Daria <daskuncik@gmail.com>
2017-09-21 18:21:45 +0300

    hello-text changed

--- Web_app/Web_app/Startup.cs
+++ Web_app/Web_app/Startup.cs
@@ -27,7 +27,7 @@ namespace Web_app
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello my favorite World!");
             });
         }
     }
